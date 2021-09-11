f25f779869f91039d29e0a3e51240420eeffa9e7
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-09-12 06:02:53 +0900

    Create DbContext

---
+++ CommandAPISolution/CommandAPI/Data/CommandContext.vb
@@ -0,0 +1,17 @@
+﻿Imports CommandAPI.Models
+Imports Microsoft.EntityFrameworkCore
+
+Namespace CommandAPI.Data
+
+    Public Class CommandContext
+        Inherits DbContext
+
+        Public Property CommandItems As DbSet(Of Command)
+
+        Public Sub New(options As DbContextOptions)
+            MyBase.New(options)
+        End Sub
+
+
+    End Class
+End Namespace
