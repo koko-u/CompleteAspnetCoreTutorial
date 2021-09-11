cf4b24eed1868897c6d19f0a60d6f3b6fa11f4a8
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2021-09-12 02:02:11 +0900

    Add Command Model

---
+++ CommandAPISolution/CommandAPI/Models/Command.vb
@@ -0,0 +1,22 @@
+﻿Imports System.ComponentModel.DataAnnotations
+
+Namespace CommandAPI.Models
+
+    Public Class Command
+
+        <Key>
+        <Required>
+        Public Property Id As Integer
+
+        <Required>
+        <MaxLength(250)>
+        Public Property HowTo As String
+
+        <Required>
+        Public Property Platform As String
+
+        <Required>
+        Public Property CommandLine As String
+    End Class
+
+End Namespace
